110cd18989a77fb6f3e801d26e34d6b7a3e4bb35
grenade-Y-O-S <crmson54@gmail.com>
2021-09-03 09:16:39 +0900

    単体テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    user_name             {'山田太郎'}
+    email                 {Faker::Internet.email}
+#    password              {Faker::Internet.password(min_length: 6)}
+    password              {'123abc'}
+    password_confirmation {password}
+    last_name             {'山田'}
+    first_name            {'太郎'}
+    last_name_kana        {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birth_date            {"1995-01-01"}
+  end
+end
