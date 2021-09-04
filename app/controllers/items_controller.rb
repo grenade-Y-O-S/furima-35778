b56f6ce5bd11bc230ec8c2f345feb712cfcca3dd
grenade-Y-O-S <crmson54@gmail.com>
2021-09-04 17:11:35 +0900

    MVC基本設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :item_category_id
+    validates :item_sales_status_id
+    validates :item_fee_shipping_status_id
+    validates :item_prefecture_id
+    validates :item_scheduled_delivery_id
+    validates :item_price, numericality: {:greater_than_or_equal_to 300, :less_than_or_equal_to 9999999}
+  end
 end
