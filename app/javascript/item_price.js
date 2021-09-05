cbee275d5b301c985a330ea3c9bc8072ea538128
grenade-Y-O-S <crmson54@gmail.com>
2021-09-05 21:26:21 +0900

    販売手数料等の計算結果表示機能実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    taxPrice = Math.floor(inputValue / 10);
+    addTaxDom.innerHTML = taxPrice;
+    const profitPrice = document.getElementById("profit");
+    console.log(profitPrice);
+    profitPrice.innerHTML = Math.floor(inputValue - taxPrice);
+  })
+})
