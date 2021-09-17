041eb942f6e0799b8f46f408fc6d92211f170eb2
grenade-Y-O-S <crmson54@gmail.com>
2021-09-17 21:19:36 +0900

    テーブル保存成功

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,20 +7,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @furima_trade = FurimaTrade.new(order_params)
-#    binding.pry
     if @furima_trade.valid?
       @furima_trade.save
       return redirect_to root_path
     else
-      render order: :index
+      render item_orders_path
     end
   end
 
   private
 
   def order_params
-    params.require(:furima_trade).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:furima_trade).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/furima_trade.rb
+++ app/models/furima_trade.rb
@@ -1,22 +1,20 @@
 class FurimaTrade
   include ActiveModel::Model
-#  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :address
-    validates :building
-    validates :phone_number
+    validates :phone_number, length: { in: 10..11 }, format: { with: /\d+/ }
     validates :user_id
     validates :item_id
-#    validates :token
   end
 
   def save
-    order = Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number)
-    trading_management = TradingManagement.create(user_id: current_user.id, item_id: item.id)
+    trading_management = TradingManagement.create(user_id: user_id, item_id: item_id)
+    order = Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, trading_management_id: trading_management.id)
   end
 end
